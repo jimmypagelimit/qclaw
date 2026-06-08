4fd59fab6b2d44753d49205bc8be872a03dce165
qujt <15206651142@163.com>
2026-06-08 22:02:05 +0800

    chore: clean up temp scripts and session state

--- _check_rym.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(r'C:\Users\qujt\.qclaw\workspace\_music_latest.db')
-cur = conn.cursor()
-cur.execute('SELECT album_name, artist, rym_rating FROM albums WHERE album_name="Twin Fantasy"')
-print(cur.fetchone())
-cur.execute('SELECT album_name, artist, rym_rating FROM albums WHERE rym_rating IS NOT NULL LIMIT 3')
-print(cur.fetchall())
-conn.close()
